aa563f9aee3580fbd87c2a3abc19635a6ca8619f
takahashi-masato <utterly.dark.chaser@gmail.com>
2020-07-26 21:24:25 +0900

    comit

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,11 +3,10 @@
 lock '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'chat-space2'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:takahashi-masato
-/chat-space2.git'
+set :repo_url,  'git@github.com:takahashi-masato/chat-space2.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
