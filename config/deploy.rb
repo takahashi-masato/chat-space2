406c576324fb89f7f98d85ff51bd7e117d603d94
takahashi-masato <utterly.dark.chaser@gmail.com>
2020-07-27 17:24:05 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'chat-space'
+set :application, 'chat-space2'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:takahashi-masato/chat-space2.git'
